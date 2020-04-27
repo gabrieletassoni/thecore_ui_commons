2b59721361d0e65e217285e7fd58207bc8893993
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2020-04-27 17:26:21 +0200

    Add migrations from this source

--- lib/thecore_ui_commons/engine.rb
+++ lib/thecore_ui_commons/engine.rb
@@ -1,4 +1,12 @@
 module ThecoreUiCommons
   class Engine < ::Rails::Engine
+    initializer 'thecore_ui_commons.add_to_migrations' do |app|
+      unless app.root.to_s.match root.to_s
+        # APPEND TO MAIN APP MIGRATIONS FROM THIS GEM
+        config.paths['db/migrate'].expanded.each do |expanded_path|
+          app.config.paths['db/migrate'] << expanded_path
+        end
+      end
+    end
   end
 end
--- lib/thecore_ui_commons/version.rb
+++ lib/thecore_ui_commons/version.rb
@@ -1,3 +1,3 @@
 module ThecoreUiCommons
-  VERSION = '2.1.1'
+  VERSION = '2.1.2'
 end
