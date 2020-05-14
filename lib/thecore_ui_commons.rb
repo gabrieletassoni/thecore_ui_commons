49c7c1d27e62e52d2c71a61703c90b5140108565
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2020-05-14 19:27:22 +0200

    place the require on engine in the right place

--- lib/thecore_ui_commons.rb
+++ lib/thecore_ui_commons.rb
@@ -1,8 +1,8 @@
-require "thecore_ui_commons/engine"
-
 require 'thecore_backend_commons'
 # Rails
 require 'serviceworker-rails'
+
+require "thecore_ui_commons/engine"
 
 module ThecoreUiCommons
   # Your code goes here...
