ff44a1c9d3c06cc741147943fde602db6af5af90
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2020-05-22 11:00:48 +0200

    add username validations

--- lib/concerns/thecore_ui_commons_user.rb
+++ lib/concerns/thecore_ui_commons_user.rb
@@ -5,6 +5,8 @@ module ThecoreUiCommonsUser
     
     included do
         devise :database_authenticatable, :trackable, :validatable, :rememberable, :timeoutable, timeout_in: 30.minutes, authentication_keys: [:login]
+        validates :username, uniqueness: { case_sensitive: false }, presence: true, length: { in: 4..15 }
+        validates_format_of :username, with: /\A[a-zA-Z0-9]*\z/, on: :create, message: "can only contain letters and digits"
         
         attr_writer :login
         
