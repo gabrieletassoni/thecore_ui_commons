1946656290f35476e22cb44d99987c3e53b9717a
Gabriele Tassoni <gabrieletassoni@alchemic.it>
2021-08-25 14:28:54 +0000

    Try to remove depedencies on thecore

--- app/assets/javascripts/thecore_ui_commons/thecore.js
+++ app/assets/javascripts/thecore_ui_commons/thecore.js
@@ -1,7 +1,7 @@
 //= require jquery
 //= require jquery-ui
+//= require jquery-ui/widget
 //= require jquery-ui/widgets/dialog
-//= require jquery_ujs
 //= require bootstrap
 //= require ie
 //= require timer
