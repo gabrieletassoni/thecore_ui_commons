d52166cc023f5a1a5d6cffe11f5741c3a84c291b
Gabriele Tassoni <g.tassoni@bancolini.com>
2022-09-26 09:34:58 +0000

    Add White label for views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :setup_white_label
+
+  private
+
+  def setup_white_label
+    # Somewhat white label support
+    prepend_view_path(["vendor/custombuilds/#{ENV['COMPOSE_PROJECT_NAME']}.#{ENV['BASE_DOMAIN']}/deltas/app/views"]) if ENV['COMPOSE_PROJECT_NAME'].present? && ENV['BASE_DOMAIN'].present?
+  end
 end
