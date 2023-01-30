df5d6c277029a6c6db1e8a9363e3b9aa1e6d8858
Gabriele Tassoni <g.tassoni@bancolini.com>
2023-01-30 13:09:03 +0000

    Yeah! Ce l'ho fatta sul layout per devise

--- config/initializers/thecore_ui_commons_assets.rb
+++ config/initializers/thecore_ui_commons_assets.rb
@@ -1,3 +1,5 @@
 Rails.application.configure do
     config.assets.precompile += %w( thecore_ui_commons_manifest.js )
+    config.assets.precompile += %w( android-chrome-192x192.png android-chrome-512x512.png )
+    config.assets.precompile += %w( devise/sessions.css devise/sessions.js )
 end
