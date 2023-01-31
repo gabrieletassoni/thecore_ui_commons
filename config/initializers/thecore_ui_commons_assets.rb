fbfe8edb2407154500a315d7149066ca6240574d
Gabriele Tassoni <g.tassoni@bancolini.com>
2023-01-31 00:03:03 +0000

    Make login page look ok!

--- config/initializers/thecore_ui_commons_assets.rb
+++ config/initializers/thecore_ui_commons_assets.rb
@@ -1,5 +1,11 @@
 Rails.application.configure do
-    config.assets.precompile += %w( thecore_ui_commons_manifest.js )
-    config.assets.precompile += %w( android-chrome-192x192.png android-chrome-512x512.png )
+    config.assets.precompile += %w( 
+        android-chrome-192x192.png 
+        android-chrome-512x512.png 
+        apple-touch-icon.png
+        favicon.ico
+        customer_logo.png
+        logo.png
+    )
     config.assets.precompile += %w( devise/sessions.css devise/sessions.js )
 end
