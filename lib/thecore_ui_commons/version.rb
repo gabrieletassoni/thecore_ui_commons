8a9ee432c4e2b291fbbc18add50734e1092b22e8
Gabriele Tassoni <g.tassoni@bancolini.com>
2023-02-06 17:36:09 +0000

    Put gem version on git tag

--- lib/thecore_ui_commons/version.rb
+++ lib/thecore_ui_commons/version.rb
@@ -1,3 +1,3 @@
 module ThecoreUiCommons
-  VERSION = "3.0.0" #"#{`git describe --tags $(git rev-list --tags --max-count=1)`.chomp}"
+  VERSION = "#{`git describe --tags $(git rev-list --tags --max-count=1)`.chomp}"
 end
