b1c0bd7162427907b6c0e40bf49691b7d1fb0c50
Gabriele Tassoni <g.tassoni@bancolini.com>
2024-09-11 15:03:38 +0000

    Remove deprecated raise_in_transactional_callbacks

--- config/initializers/application_config.rb
+++ config/initializers/application_config.rb
@@ -1,6 +1,6 @@
 Rails.application.config.filter_parameters += [:password]
 
-Rails.application.config.active_record.raise_in_transactional_callbacks = true
+# Rails.application.config.active_record.raise_in_transactional_callbacks = true
 
 Rails.application.config.assets.configure do |env|
     env.export_concurrent = false
