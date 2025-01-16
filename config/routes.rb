cb73f148943649cf44e54b47285e693a3312e534
Gabriele Tassoni <g.tassoni@bancolini.com>
2025-01-16 16:19:32 +0000

    Manage sub-path URIs

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-    devise_for :users
-    devise_scope :user do
-        root to: "devise/sessions#new"
-    end
+    scope ENV.fetch("RAILS_RELATIVE_URL_ROOT", "/") do
+        devise_for :users
+        devise_scope :user do
+            root to: "devise/sessions#new"
+        end
 
-    get '/info/swagger', to: 'info#swagger'
+        get '/info/swagger', to: 'info#swagger'
+    end
 end
