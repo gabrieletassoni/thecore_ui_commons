68fbfd048b410533d1c819062660856831785d60
Gabriele Tassoni <g.tassoni@bancolini.com>
2025-01-17 16:41:14 +0000

    Step by step debug for sub-uri

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-    # scope ENV.fetch("RAILS_RELATIVE_URL_ROOT", "/") do
+    scope ENV.fetch("RAILS_RELATIVE_URL_ROOT", "/") do
         devise_for :users
-        devise_scope :user do
+    end
+    
+    devise_scope :user do
+        scope ENV.fetch("RAILS_RELATIVE_URL_ROOT", "/") do
             root to: "devise/sessions#new"
         end
+    end
 
+    scope ENV.fetch("RAILS_RELATIVE_URL_ROOT", "/") do
         get '/info/swagger', to: 'info#swagger'
-    # end
+    end
 end
