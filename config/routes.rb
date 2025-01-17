28c7e697c110e9b9367483e2a3476f4d0311b270
Gabriele Tassoni <g.tassoni@bancolini.com>
2025-01-17 08:38:05 +0000

    Remove scoping for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-    scope ENV.fetch("RAILS_RELATIVE_URL_ROOT", "/") do
+    # scope ENV.fetch("RAILS_RELATIVE_URL_ROOT", "/") do
         devise_for :users
         devise_scope :user do
             root to: "devise/sessions#new"
         end
 
         get '/info/swagger', to: 'info#swagger'
-    end
+    # end
 end
