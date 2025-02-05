7226a222081fca4d81763d735f59b069848a021c
Gabriele Tassoni <g.tassoni@bancolini.com>
2025-02-05 10:43:39 +0000

    Use svg for logos

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,10 +1,6 @@
-Rails.application.config.assets.precompile += %w( 
-    android-chrome-192x192.png 
-    android-chrome-512x512.png 
-    apple-touch-icon.png
-    favicon.ico
-    customer_logo.png
-    logo.png
+Rails.application.config.assets.precompile += %w(
+    app_logo.svg
+    logo.svg
     devise/sessions.css 
     devise/sessions.js
     thecore_ui_commons.js
--- lib/thecore_ui_commons/version.rb
+++ lib/thecore_ui_commons/version.rb
@@ -1,3 +1,3 @@
 module ThecoreUiCommons
-  VERSION = "3.2.16".freeze
+  VERSION = "3.2.17".freeze
 end
