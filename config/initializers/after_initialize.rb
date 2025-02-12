92a0e198f635eb35eb924078f5596e16ed788ea9
Gabriele Tassoni <g.tassoni@bancolini.com>
2025-02-12 10:50:51 +0000

    Debug api request auth chain: remove unused user concern and put it in thecore_auth_commons

--- config/initializers/after_initialize.rb
+++ config/initializers/after_initialize.rb
@@ -2,6 +2,5 @@ Rails.application.configure do
     config.after_initialize do
         # Controllers
         ApplicationController.send(:include, ConcernCommonApplicationController)
-        User.send(:include, ThecoreUiCommonsUserConcern)
     end
 end
